9135a425f55d9937b737023b370cfa4e31cae49f
VincentDSZ <65813931+VincentDSZ@users.noreply.github.com>
2024-09-16 11:08:02 +0200

    Leerling migrations

---
+++ database/migrations/2024_09_16_084310_create_leerlingen_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leerlingen', function (Blueprint $table) {
+            $table->id();
+            $table->string('voornaam');
+            $table->string('achternaam');
+            $table->date('geboortedatum');
+            $table->string('diploma');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leerlingen');
+    }
+};
