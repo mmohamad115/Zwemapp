544a664bc571d6b2f74c164e7a48a5ab9bfb1596
milagro2 <morenoseijs06@gmail.com>
2024-09-16 11:16:59 +0200

    feedback seeder+model created

---
+++ database/seeders/FeedbackSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Feedback;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FeedbackSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Feedback::create([
+            'leerling_id' => 1,
+            'zwem_docent_id' => 1,
+            'content' => 'Great progress in swimming!',
+            'aanmaakdatum' => now()
+        ]);
+
+        Feedback::create([
+            'leerling_id' => 2,
+            'zwem_docent_id' => 2,
+            'content' => 'Needs improvement in backstroke.',
+            'aanmaakdatum' => now()
+        ]);
+    }
+}
