919d8282e2b66a8b93146b96b017223358832ace
milagro2 <morenoseijs06@gmail.com>
2024-10-09 11:14:35 +0200

    made navbar & sidebar in another file

--- resources/views/zwemdocenten/showleerling.blade.php
+++ resources/views/zwemdocenten/showleerling.blade.php
@@ -12,88 +12,19 @@
     <script src="https://kit.fontawesome.com/db1f6bf93f.js" crossorigin="anonymous"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-    <p><strong>Voornaam:</strong> {{ $leerling->voornaam }}</p>
-    <p><strong>Achternaam:</strong> {{ $leerling->achternaam }}</p>
-    <p><strong>Geboortedatum:</strong> {{ $leerling->geboortedatum }}</p>
-    <p><strong>Diploma:</strong> {{ $leerling->diploma }}</p>
 
-    <p><strong>Voortgang:</strong> {{ $leerling->lessons_completed }} / {{ $totalLessons }}</p>
-
-    <p><strong>Feedback:</strong></p>
-    <ul>
-        @foreach ($leerling->feedback as $feedback)
-            <li>{{ $feedback->content }} ({{ $feedback->aanmaakdatum }})</li>
-        @endforeach
-    </ul>
 
 <body class="overflow-x-hidden bg-gray-100">
     <div class="">
-        <nav class="flex bg-white text-gray-950 ml-16 absolute w-full rounded-b-xl z-50">
-            <div class="px-5 xl:px-12  flex w-full items-center">
-                <a class="text-3xl font-bold font-heading">
-                    <img class="h-20 w-20" src="{{ asset('photos/SSClogoText.png') }}" alt="logo">
-                    {{-- SplashZone Swim Center --}}
-                </a>
-                <ul class="flex px-4 mx-auto font-semibold font-heading space-x-12">
-                    <li><a class="hover:text-cyan-400" href="{{ route('profile.edit') }}">Dashboard</a></li>
-                    <li><a class="hover:text-cyan-400" href="#">Tijden</a></li>
-                    <li><a class="hover:text-cyan-400" href="#">Tarieven</a></li>
-                    <li><a class="hover:text-cyan-400" href="#">Contact Us</a></li>
-                </ul>
-                <div class="items-center space-x-5 items-center mr-14">
-                    <div class="items-center space-x-5 items-center flex">
-                        <div class="flex items-center space-x-2">
-                            <span class="text-gray-900">{{ auth()->user()->name }}</span>
-                            <i class="fas fa-user-circle text-gray-900 text-2xl"></i>
-                        </div>
-                        <form method="POST" action="{{ route('logout') }}" class=" items-center ">
-                            @csrf
-                            <button type="submit" class="bg-red-600 text-white hover:bg-red-700 px-2 py-1 rounded-lg">
-                                <i class="fas fa-sign-out-alt text-xs"></i>
-                                Logout
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </nav>
+        @include('components.navbar')
+
         <div class="bg-white pr-4 pt-2 flex items-center justify-between h-20">
             <div class="flex items-center space-x-4">
             </div>
         </div>
     </div>
     <div class="flex">
-        <aside class="bg-cyan-400 text-white w-64 h-screen p-4 overflow-y-auto relative">
-            <nav>
-                <ul class="space-y-2">
-                    <li class="opcion-con-desplegable">
-                        <div class="flex items-center justify-between p-2 hover:bg-cyan-300 cursor-pointer rounded-lg">
-                            <div class="flex items-center">
-                                <i class="fa-solid fa-person-swimming mr-2"></i>
-                                <a href="{{ route('zwemlessen.index') }}">Zwemlessen</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="opcion-con-desplegable">
-                        <div class="flex items-center justify-between p-2 hover:bg-cyan-300 cursor-pointer rounded-lg">
-                            <div class="flex items-center">
-                                <i class="fa-solid fa-person-chalkboard mr-2"></i>
-                                <a href="{{ route('zwemlessen.create') }}">Toevoegen les</a>
-                            </div>
-                        </div>
-                    </li>
-                    </li>
-                    <li class="opcion-con-desplegable">
-                        <div class="flex items-center justify-between p-2 hover:bg-cyan-300 cursor-pointer rounded-lg">
-                            <div class="flex items-center">
-                                <i class="fa-solid fa-children mr-2"></i>
-                                <a href="{{ route('leerlingen.index') }}">Mijn leerlingen</a>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </nav>
-        </aside>
+        @include('components.sidebar')
         <div class="w-full">
             <div class="flex">
                 <table class="m-10 w-full shadow-xl rounded-xl text-sm text-left rtl:text-right text-white">
